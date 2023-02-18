87800721b04fa3b2a69bd8cfb0ca50520b77dcdd
hzd1719 <panayot.2k@gmail.com>
2023-02-18 20:10:08 +0200

    moved calculate code to GroceryShop

--- TheGroceryShop/src/main/java/org/example/GroceryShop.java
+++ TheGroceryShop/src/main/java/org/example/GroceryShop.java
@@ -25,7 +25,6 @@ public class GroceryShop {
         return false;
     }
 
-
     public boolean setSpecialDeal(Groceries grocery) {
         if(availableGroceries.containsKey(grocery)) {
             this.specialDealItem = grocery;
@@ -51,17 +50,125 @@ public class GroceryShop {
         if(groceryShopTillList.contains(groceryShopTill)) {
             for(Groceries grocery : groceries) {
                 groceryShopTill.scanGrocery(grocery);
-            };
-            return groceryShopTill.finalPrice(this.availableGroceries, this.twoForThreeDealItems, TWO_FOR_THREE_DEAL_CAPACITY,
-                    this.specialDealItem, getPriceOfAGrocery(this.specialDealItem));
+            }
+            return finalPrice(groceryShopTill);
         }
 
         else {
             return "Wrong grocery till!";
         }
     }
+
+
+    //get the final price
+    public String finalPrice(GroceryShopTill groceryShopTill) {
+        BigDecimal price = calculateFinalPrice(groceryShopTill);
+        return priceToStringStatement(price, "aws", "clouds");
+    }
+
+    //method to calculate the final price
+    private BigDecimal calculateFinalPrice(GroceryShopTill groceryShopTill) {
+        BigDecimal price = getPriceWithoutASale(groceryShopTill);
+        //reduce price only if there is a special deal
+        if(this.specialDealItem != null) {
+            price = finalizeSpecialDeal(groceryShopTill, price);
+        }
+        //reduce price only if there is a properly set two for three deal
+        if(this.twoForThreeDealItems.size() == TWO_FOR_THREE_DEAL_CAPACITY) {
+            price = finalizeTwoForThreeDeal(groceryShopTill, price);
+        }
+
+        return price;
+    }
+
+    //get the price before applying any sales
+    private BigDecimal getPriceWithoutASale(GroceryShopTill groceryShopTill) {
+        BigDecimal price = BigDecimal.ZERO;
+        for(Groceries grocery : groceryShopTill.getGroceriesListTill()) {
+            price = price.add(availableGroceries.get(grocery));
+
+        }
+        return price;
+    }
+
+    //From the first three scanned items from the two for three deal get the cheapest item
+    private BigDecimal cheapestItemFromTwoForThreeDeal(GroceryShopTill groceryShopTill) {
+        BigDecimal min = this.availableGroceries.get(this.twoForThreeDealItems.get(0));
+        int count = 0;
+        for (Groceries twoForThreeEl : twoForThreeDealItems) {
+            for (Groceries grocery : groceryShopTill.getGroceriesListTill()) {
+                if (grocery.equals(twoForThreeEl)) {
+                    count++;
+                    if (min.compareTo(availableGroceries.get(grocery)) > 0) {
+                        min = availableGroceries.get(grocery);
+                    }
+                }
+                if (count == TWO_FOR_THREE_DEAL_CAPACITY) {
+                    break;
+                }
+            }
+        }
+
+        return min;
+    }
+
+    // reduce the price by the cheapest item of the two for three deal
+    private BigDecimal reducePriceTwoForThreeDeal(GroceryShopTill groceryShopTill, BigDecimal price) {
+        price = price.subtract(cheapestItemFromTwoForThreeDeal(groceryShopTill));
+        return price;
+    }
+
+    //if the customer has bought all the three products from the sale, reduce his final price by the cheapest item from the first three scanned
+    private BigDecimal finalizeTwoForThreeDeal(GroceryShopTill groceryShopTill, BigDecimal price) {
+        if (groceryShopTill.getGroceriesListTill().containsAll(this.twoForThreeDealItems)) {
+            price = reducePriceTwoForThreeDeal(groceryShopTill, price);
+        }
+        return price;
+    }
+
+    //count the number of special deal items the client wants to buy
+    private int countSpecialDealItemsByName(GroceryShopTill groceryShopTill) {
+        int specialItemCount = 0;
+        for (Groceries grocery : groceryShopTill.getGroceriesListTill()) {
+            if (grocery.equals(specialDealItem)) {
+                specialItemCount++;
+            }
+        }
+        return specialItemCount;
+    }
+
+    //reduce the price by reduceBy times of every second special item
+    private BigDecimal reducePriceSpecialDeal(BigDecimal price, int quantityOfSpecialItem, int reduceBy) {
+        price = price.subtract(
+                (availableGroceries.get(this.specialDealItem).divide(BigDecimal.valueOf(reduceBy)))
+                        .multiply(BigDecimal.valueOf(quantityOfSpecialItem))
+        );
+
+        return price;
+    }
+
+
+    //reduce the price by 50% of the value of every second special deal item the customer has bought
+    private BigDecimal finalizeSpecialDeal(GroceryShopTill groceryShopTill, BigDecimal price) {
+        //reduce price only if the customer has bought a special deal item
+        if (groceryShopTill.getGroceriesListTill().contains(this.specialDealItem)) {
+            int specialItemCount = countSpecialDealItemsByName(groceryShopTill);
+            //Every second specialItem gets a 50% discount, so we divide all the specialItems by two
+            specialItemCount /= 2;
+            price = reducePriceSpecialDeal(price, specialItemCount, 2);
+        }
+        return price;
+    }
+
+    //Method to transform the price to a string statement of it's cost
+    private String priceToStringStatement(BigDecimal price, String currency, String subCurrency) {
+        String[] currencyStrArr = String.valueOf(price).split("\\.");
+        return String.valueOf(String.format("%s %s and %s %s", currencyStrArr[0], currency, currencyStrArr[1], subCurrency));
+    }
+
+
     public static int getCapacity() {
-        return GroceryShop.TWO_FOR_THREE_DEAL_CAPACITY;
+        return org.example.GroceryShop.TWO_FOR_THREE_DEAL_CAPACITY;
     }
 
     public HashMap<Groceries, BigDecimal> getAvailableGroceries() {
@@ -84,4 +191,5 @@ public class GroceryShop {
         return this.availableGroceries.get(grocery);
     }
 }
+
 
--- TheGroceryShop/src/main/java/org/example/GroceryShopTill.java
+++ TheGroceryShop/src/main/java/org/example/GroceryShopTill.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import java.math.BigDecimal;
 import java.util.*;
 
 public class GroceryShopTill {
-    private List<Groceries> groceriesListTill = new ArrayList<>();
-    private int number;
+    private final List<Groceries> groceriesListTill = new ArrayList<>();
+    private final int number;
 
     public GroceryShopTill(int number) {
         this.number = number;
@@ -22,110 +21,6 @@ public class GroceryShopTill {
         }
     }
 
-    //get the final price
-    public String finalPrice(Map<Groceries, BigDecimal> availableGroceries, List<Groceries> twoForThreeDealList, int maxCapacity,
-                             Groceries specialDealItem, BigDecimal specialDealItemPrice) {
-        BigDecimal price = getPriceWithoutASale(availableGroceries);
-        //reduce price only if there is a special deal
-        if(specialDealItem != null) {
-            price = finalizeSpecialDeal(price, specialDealItem, specialDealItemPrice);
-        }
-       if(twoForThreeDealList.size() == maxCapacity) {
-            price = finalizeTwoForThreeDeal(availableGroceries, twoForThreeDealList, maxCapacity, price);
-        }
-        return priceToStringStatement(price, "aws", "clouds");
-    }
-
-    //get the price before applying any sales
-    private BigDecimal getPriceWithoutASale(Map<Groceries, BigDecimal> availableGroceries) {
-        BigDecimal price = BigDecimal.ZERO;
-        for(Groceries grocery : groceriesListTill) {
-            price = price.add(availableGroceries.get(grocery));
-
-        }
-        return price;
-    }
-
-    //From the first three scanned items from the two for three deal get the cheapest item
-    private BigDecimal cheapestItemFromTwoForThreeDeal(Map<Groceries, BigDecimal> availableGroceries,  List<Groceries> twoForThreeDealList, int maxCapacity) {
-        BigDecimal min = availableGroceries.get(twoForThreeDealList.get(0));
-        int count = 0;
-
-        for (Groceries twoForThreeEl : twoForThreeDealList) {
-            for (Groceries grocery : this.groceriesListTill) {
-                if (grocery.equals(twoForThreeEl)) {
-                    count++;
-                    if (min.compareTo(availableGroceries.get(grocery)) > 0) {
-                        min = availableGroceries.get(grocery);
-                    }
-                }
-                if (count == maxCapacity) {
-                    break;
-                }
-            }
-        }
-
-        return min;
-    }
-
-    // reduce the price by the cheapest item of the two for three deal
-    private BigDecimal reducePriceTwoForThreeDeal(Map<Groceries, BigDecimal> availableGroceries,  List<Groceries> twoForThreeDealList,
-                                                  int maxCapacity, BigDecimal price) {
-        price = price.subtract(cheapestItemFromTwoForThreeDeal(availableGroceries, twoForThreeDealList, maxCapacity));
-        return price;
-    }
-
-    //if the customer has bought all the three products from the sale, reduce his final price by the cheapest item from the first three scanned
-    private BigDecimal finalizeTwoForThreeDeal(Map<Groceries, BigDecimal> availableGroceries,  List<Groceries> twoForThreeDealList,
-                                               int maxCapacity, BigDecimal price) {
-        if (this.groceriesListTill.containsAll(twoForThreeDealList)) {
-            price = reducePriceTwoForThreeDeal(availableGroceries, twoForThreeDealList, maxCapacity, price);
-        }
-        return price;
-    }
-
-
-
-    //count the number of special deal items the client wants to buy
-    private int countSpecialDealItemsByName(Groceries specialDealItem) {
-        int specialItemCount = 0;
-        for (Groceries grocery : groceriesListTill) {
-            if (grocery.equals(specialDealItem)) {
-                specialItemCount++;
-            }
-        }
-        return specialItemCount;
-    }
-
-    //reduce the price by reduceBy times of every second special item
-    private BigDecimal reducePriceSpecialDeal(BigDecimal price, int quantityOfSpecialItem, int reduceBy, BigDecimal specialDealItemPrice) {
-        price = price.subtract(
-                (specialDealItemPrice.divide(BigDecimal.valueOf(reduceBy)))
-                        .multiply(BigDecimal.valueOf(quantityOfSpecialItem))
-        );
-
-        return price;
-    }
-
-
-    //reduce the price by 50% of the value of every second special deal item the customer has bought
-    private BigDecimal finalizeSpecialDeal(BigDecimal price, Groceries specialDealItem, BigDecimal specialDealItemPrice) {
-        //reduce price only if the customer has bought a special deal item
-        if (this.groceriesListTill.contains(specialDealItem)) {
-            int specialItemCount = countSpecialDealItemsByName(specialDealItem);
-            //Every second specialItem gets a 50% discount, so we divide all the specialItems by two
-            specialItemCount /= 2;
-            price = reducePriceSpecialDeal(price, specialItemCount, 2, specialDealItemPrice);
-        }
-        return price;
-    }
-
-    //Method to transform the price to a string statement of it's cost
-    private String priceToStringStatement(BigDecimal price, String currency, String subCurrency) {
-        String[] currencyStrArr = String.valueOf(price).split("\\.");
-        return String.valueOf(String.format("%s %s and %s %s", currencyStrArr[0], currency, currencyStrArr[1], subCurrency));
-    }
-
 
     public List<Groceries> getGroceriesListTill() {
         return groceriesListTill;
@@ -139,7 +34,7 @@ public class GroceryShopTill {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        GroceryShopTill that = (GroceryShopTill) o;
+        org.example.GroceryShopTill that = (org.example.GroceryShopTill) o;
         return number == that.number;
     }
 
